4186b60aed3353bf3358c48f6145e5b9e22a73a9
Victor Tavares <victavares2019@gmail.com>
2025-07-07 14:19:37 -0300

    feat: starting implementing use case

--- src/data/use_cases/user_finder.py
+++ src/data/use_cases/user_finder.py
@@ -9,4 +9,22 @@ class UserFinder(UserFinderInterface):
         self.__users_repository =  users_repository
 
     def find(self, first_name:str) -> Dict:
-        pass
+
+        if not first_name.isalpha():
+            raise Exception('Name is invalid to search')
+
+        if len(first_name) > 18:
+            raise Exception('Name is too long to search')
+
+        users = self.__users_repository.select_user(first_name)
+
+        if not users:
+            raise Exception('User not found')
+
+        response = {
+            "type": 'Users',
+            "count": len(users),
+            "attributes" : users
+        }
+
+        return response
--- tests/user_finder_test.py
+++ tests/user_finder_test.py
@@ -1,6 +1,16 @@
 from src.data.use_cases.user_finder import UserFinder
-from src.infra.db.repositories.users_repository import UsersRepository
+from src.infra.db.tests.user_repository import UsersRepositorySpy
 
 def test_find():
-    repo = UsersRepository()
+    first_name = 'meunome'
+
+    repo = UsersRepositorySpy()
     user_finder = UserFinder(repo)
+
+    response = user_finder.find(first_name)
+
+    assert repo.select_user_attributes['first_name'] == first_name
+
+    assert response ['type'] == 'Users'
+    assert response ['count'] == len(response['attributes'])
+    assert response ['attributes'] != []
