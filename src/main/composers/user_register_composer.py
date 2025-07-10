a520aaf50610fb4f02cf093e6e5a4edaa6705e3f
Victor Tavares <victavares2019@gmail.com>
2025-07-10 16:07:09 -0300

    feat: starting create composers

---
+++ src/main/composers/user_register_composer.py
@@ -0,0 +1,6 @@
+from src.infra.db.repositories.users_repository import UsersRepository
+from src.data.use_cases.user_register import UserRegister
+from src.presentation.controllers.user_register_controller import UserRegisterController
+
+def user_register_composer():
+    pass
