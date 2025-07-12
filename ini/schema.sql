2389c6512ee3a1080eed2eac15a719f99c74f3b0
victo29 <victavares2019@gmail.com>
2025-07-11 21:44:35 -0300

    config: change varchar value

--- ini/schema.sql
+++ ini/schema.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE IF NOT EXISTS users (
     id BIGSERIAL PRIMARY KEY,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
+    first_name VARCHAR(18) NOT NULL,
+    last_name VARCHAR(18) NOT NULL,
     age BIGINT NOT NULL
 );
